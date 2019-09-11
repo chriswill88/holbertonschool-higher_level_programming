e5967464f2663d84905e32fd13adb1246e4edcdd
chriswill88 <williechri79@gmail.com>
2019-09-11 02:15:11 +0000

    task 4 complete

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+x = 0
+it = 0
+asci = 97
+for i in range(0, 98):
+
+	if x == 15:
+		asci=97
+		x = 0
+		it = it + 1
+	else:
+		x = x + 1
+	if x > 9:
+		char=chr(asci)
+		asci = asci + 1	
+	else:
+		char = x
+	i = i + 1
+
+	print("{} = {}x{}{}".format(i, 0, it, char))
