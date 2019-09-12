6801853d8ecf103df17a8f23a711371d763f652a
chriswill88 <williechri79@gmail.com>
2019-09-12 17:15:09 -0400

    task 2 fix: wont run if being imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 from sys import argv
 
-dot = ":"
-plural = "arguments"
-argc = len(argv) - 1
+if __name__ == "__main__":
+    dot = ":"
+    plural = "arguments"
+    argc = len(argv) - 1
 
-if argc == 0:
-    dot = "."
-if argc == 1:
-    plural = "argument"
+    if argc == 0:
+        dot = "."
+    if argc == 1:
+        plural = "argument"
 
-print("{} {}{}".format(argc, plural, dot))
+    print("{} {}{}".format(argc, plural, dot))
 
-if argc > 0:
-    for i in range(1, argc + 1):
-        print("{}: {}".format(i, argv[i]))
-        i += 1
+    if argc > 0:
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, argv[i]))
+            i += 1
