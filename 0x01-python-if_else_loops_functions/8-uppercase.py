3e51cd1ee533b92ccfe9b79865b724a13a236f14
chriswill88 <williechri79@gmail.com>
2019-09-12 00:58:02 -0400

    task 9

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    i = 0
+    while i < len(str):
+        if ord(str[i]) in range(97, 122):
+            a = chr((ord(str[i]) - ord('a')) + ord('A'))
+        else:
+            a = str[i]
+        print(a, end="")
+        i += 1
+    print()
