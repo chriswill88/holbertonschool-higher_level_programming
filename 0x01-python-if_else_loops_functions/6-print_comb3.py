06c2e9adc8616fbddfc0452fa5e5055b8f9596ea
chriswill88 <williechri79@gmail.com>
2019-09-11 23:59:58 -0400

    task 6 fix :right output

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-for i in range(0, 9):
-    for x in range(0, 9):
+for i in range(0, 10):
+    for x in range(0, 10):
         if x > i:
-            print("{:02d}".format(i), end=", ")
+            print("{:2d}{:d}".format(i, x), end="")
+        if x < 10 and i < 8 and x > i:
+            print(", ", end="")
+print()
