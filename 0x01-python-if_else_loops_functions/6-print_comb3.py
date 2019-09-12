cec16f65a1c98c285d5420501438cbbd7add7a67
chriswill88 <williechri79@gmail.com>
2019-09-12 00:02:11 -0400

    task 6 fix :right output, no 2 spaces

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for i in range(0, 10):
     for x in range(0, 10):
         if x > i:
-            print("{:2d}{:d}".format(i, x), end="")
+            print("{:d}{:d}".format(i, x), end="")
         if x < 10 and i < 8 and x > i:
             print(", ", end="")
 print()
