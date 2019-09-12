9d355a4e7dfd80f6ccc27c3fbdda626a3ba5f49b
chriswill88 <williechri79@gmail.com>
2019-09-12 01:26:18 -0400

    9 task

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_last_digit(number):
+    """prints and returns the last digit"""
+    a = str(number)[len(str(number)) - 1]  # converts an int into a string
+    print("{}".format(a), end="")  # prints the last digit
+    return a  # returns the last digit
