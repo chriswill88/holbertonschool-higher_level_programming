71011d556e0f868445c529025df276d23e71ea30
chriswill88 <williechri79@gmail.com>
2019-09-12 22:11:28 +0000

    task 4: pep 8 compliant

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,8 +5,6 @@ if __name__ == "__main__":
     hold = dir(hidden_4)
     length = len(hold)
     for i in range(0, length):
-        if hold[i][0] != '_': 
-    	    print("{}".format(hold[i]))
+        if hold[i][0] != '_':
+            print("{}".format(hold[i]))
     i += 1
-
-
