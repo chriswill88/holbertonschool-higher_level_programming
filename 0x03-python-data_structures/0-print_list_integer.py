f5b3e0c7c7a56563d97a95bfe42540b3436f3317
chriswill88 <williechri79@gmail.com>
2019-09-17 11:25:30 -0400

    task 0 added

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_list_integer(my_list=[]):
+    """ this function prints the list """
+    for i in range(len(my_list)):  # loop for moving through the list
+        str = "{}"
+        print(str.format(my_list[i]))
