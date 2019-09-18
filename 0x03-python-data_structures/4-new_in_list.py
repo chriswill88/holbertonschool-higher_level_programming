ef9d8585e1c4ebcde19d49a517394570a7bbbd5e
chriswill88 <williechri79@gmail.com>
2019-09-17 23:43:03 -0400

    task 4 added

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    """ creates a node and edit a paticular index"""
+    cpy = list.copy(my_list)
+    if idx < 0 or idx > len(my_list):
+        return cpy
+    cpy[idx] = element
+    return cpy
