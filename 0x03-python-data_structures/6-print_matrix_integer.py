7bd8b0cfc1d86e5f9d817290864ef445f34af113
chriswill88 <williechri79@gmail.com>
2019-09-17 23:33:13 -0400

    task 6 added

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    """ prints a matrix"""
+    i = 0
+    x = 0
+    while i in range(len(matrix)):  # looping through columns
+        while x in range(0, len(matrix[i])):  # looping through rows
+            str = "{}"
+            print(str.format(matrix[i][x]), end="")
+            if x < len(matrix[i]) - 1:  # printing spaces
+                print(" ", end="")
+            x += 1
+        print()  # printing newline
+        x = 0
+        i += 1
