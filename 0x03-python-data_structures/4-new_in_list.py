53a21e4b0c2570fdb4177aca82be30022d657d6d
chriswill88 <williechri79@gmail.com>
2019-09-17 23:49:08 -0400

    task 4 fix returns: my_list on fail

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,6 +5,6 @@ def new_in_list(my_list, idx, element):
     """ creates a node and edit a paticular index"""
     cpy = list.copy(my_list)
     if idx < 0 or idx > len(my_list):
-        return cpy
+        return my_list
     cpy[idx] = element
     return cpy
