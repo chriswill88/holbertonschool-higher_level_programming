c23f4eea01b98c6b2ce2548a6afa25609a38a05c
chriswill88 <williechri79@gmail.com>
2019-09-17 23:51:08 -0400

    fix: format for int

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,5 +4,5 @@
 def print_list_integer(my_list=[]):
     """ this function prints the list """
     for i in range(len(my_list)):  # loop for moving through the list
-        str = "{}"
-        print(str.format(my_list[i]))
+        str = "{:d}"  # str given a value for str.format
+        print(str.format(my_list[i]))  # print using str.format
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,6 +4,6 @@
 def print_reversed_list_integer(my_list=[]):
     i = len(my_list) - 1
     while i >= 0:
-        str = "{}"
+        str = "{:d}"
         print(str.format(my_list[i]))
         i -= 1
