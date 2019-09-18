6cde2373a07b764c1e08da6ac5e8502e46680af1
chriswill88 <williechri79@gmail.com>
2019-09-17 23:55:21 -0400

    no str casting

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@
 
 def element_at(my_list, idx):
     """ this function gets the element at an index """
-    if idx > len(my_list) or idx < 0:
+    if idx >= len(my_list) or idx < 0:
         return None
 
-    return str(my_list[idx])
+    return my_list[idx]
