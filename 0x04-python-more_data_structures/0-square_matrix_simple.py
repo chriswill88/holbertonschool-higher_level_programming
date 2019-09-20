2192a877953e113f0b917fcf447a5e7cc77ef875
chriswill88 <williechri79@gmail.com>
2019-09-19 23:45:25 -0400

    fix for task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 
 
 def square_matrix_simple(matrix=[]):
-    lis = [[matrix[x][y] ** 2 for y in range(3)] for x in range(3)]
+    lis = [[matrix[x][y] ** 2 for y in range(len(matrix[x]))] for x in range(len(matrix))]
     return lis
