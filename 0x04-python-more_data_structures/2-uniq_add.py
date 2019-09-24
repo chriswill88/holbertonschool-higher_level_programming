638fe25a50ca021fbe4046f323ae1b3ace45066a
chriswill88 <williechri79@gmail.com>
2019-09-23 20:37:32 -0400

    added: task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,14 +3,11 @@
 
 def uniq_add(my_list=[]):
     sum = 0
-    x = 0
-    for i in range(len(my_list)):
-        check = my_list[i]
-        x == 0
-        while x < i - 1:
-            if x < i and my_list[i] == check:
-                sum += check
-                break
-            x += 1
+    lis = []
+    for i in my_list:
+        if i not in lis:
+            lis.append(i)
+    for i in lis:
+        sum += i
 
     return sum
