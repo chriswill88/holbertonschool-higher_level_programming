453cbd3de768df4eaada3adbb550ea5847e79217
chriswill88 <williechri79@gmail.com>
2019-10-01 23:10:07 -0400

    1-square.py added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    Creating a Class: Square
+    Attributes|
+        Private Instance Variable: __size
+    """
+    def __init__(self, size):
+        self.__size = size
