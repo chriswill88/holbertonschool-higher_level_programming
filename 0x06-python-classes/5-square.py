a085ca3fd1cbeef6ff7346fcf21e4e7563afaeb1
chriswill88 <williechri79@gmail.com>
2019-10-02 17:20:06 -0400

    3-square.py added

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    Creating a Class: Square
+    Attributes|
+        Private Instance Variable: __size
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if 0 > value:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    """
+        Public instance method: area
+        returns the area based on the size
+    """
+    def area(self):
+        size = self.__size
+        return size * size
+
+    def my_print(self):
+        size = self.size
+        for i in range(size):
+            print('#' * size)
+        if size == 0:
+            print()
