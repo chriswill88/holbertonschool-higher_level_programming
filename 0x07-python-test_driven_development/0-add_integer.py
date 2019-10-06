e6b60cf8ff020350044546145d581894a7c9dfce
chriswill88 <williechri79@gmail.com>
2019-10-06 04:32:59 -0400

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 """
-
+This modual is for adding two numbers
 """
+
+
 def add_integer(a, b=98):
     """
+    add_integers - adds two numbers
+
+    a - a number
+    b - a number
+
+    Return: the sum
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return a + b
+    return int(a + b)
