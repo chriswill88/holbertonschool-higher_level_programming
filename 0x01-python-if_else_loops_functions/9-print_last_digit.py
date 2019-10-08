a01845d27edda3a625f5f774aa157a20c7c5719c
chriswill88 <williechri79@gmail.com>
2019-10-08 02:14:28 -0400

    task 9 fix: returns int

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,6 +5,6 @@ def print_last_digit(number):
     """
     prints and returns the last digit
     """
-    a = str(number)[len(str(number)) - 1]  # converts an int into a string
+    a = int(str(number)[len(str(number)) - 1])  # converts an int into a string
     print("{}".format(a), end="")  # prints the last digit
     return a  # returns the last digit
