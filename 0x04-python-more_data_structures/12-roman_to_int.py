2ded72630fbac318721710fb2beed63c19cba8db
chriswill88 <williechri79@gmail.com>
2019-10-10 06:23:16 -0400

    task 12 added

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    rs = roman_string
+    num = 0
+    romnum = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    if not isinstance(roman_string, (str)) or roman_string is None:
+        return 0
+    for x in range(len(rs)):
+        for key, val in romnum.items():
+            if rs[x] is key:
+                if x == 0 and key is 'I' and len(rs) > x:
+                    num -= val
+                else:
+                    num += val
+
+    return num
