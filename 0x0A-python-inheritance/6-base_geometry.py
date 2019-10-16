e3c3953ad1ef41b7956c336d17e584076b01f286
chriswill88 <williechri79@gmail.com>
2019-10-15 22:46:22 -0400

    task 6 completed

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This modual contains the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry
+
+    Methods:
+        area()
+    """
+    def area(self):
+        """
+        area - empty method in BaseGeometry
+        """
+        raise Exception("area() is not implemented")
