b9043c5b2c924149bb4ca01db15d6158967d6671
chriswill88 <williechri79@gmail.com>
2019-10-16 20:35:32 -0400

    task 0 fix: added end="" and took out permissions

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,9 +6,9 @@ modual function reads a text file prints to stdout
 
 def read_file(filename=""):
     """
-    function read_file - 
+    function read_file -
     reads file using with and open.
     """
-    with open(filename, 'r') as f:
+    with open(filename) as f:
         for line in f:
-            print(line)
+            print(line, end="")
