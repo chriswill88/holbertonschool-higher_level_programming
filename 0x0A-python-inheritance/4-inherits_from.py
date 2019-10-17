2fa83d49c83885751fa74bc905e2cacb076d2cb6
chriswill88 <williechri79@gmail.com>
2019-10-17 14:19:48 -0400

    task 4 fix, conditional added and pep8 compliant

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ This modual contains the program inherits from
 
 def inherits_from(obj, a_class):
     """
-    inherits_from - checks if obj is of a_class or if obj inherits from this a_class
+    inherits_from - checks if obj is of a_class
 
     Parameters:
         obj - the object
@@ -16,4 +16,4 @@ def inherits_from(obj, a_class):
         True if obj inherits from a_class
         or False if not
     """
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
