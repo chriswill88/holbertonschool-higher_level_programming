436fc323e1ff2827c3ab11a0070805fa3c86084d
chriswill88 <williechri79@gmail.com>
2019-10-18 00:19:21 -0400

    task 1

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 import sys
+import os.path
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
 listy = []
-i = 1
 argv = sys.argv
-while i < len(argv):
-    if i > 0:
-        listy.append(argv[i])
-    i += 1
+if os.path.exists('add_item.json'):
+    listy += load_from_json_file('add_item.json')
+
+
+for i in argv[1:]:
+    listy.append(i)
 
 save_to_json_file(listy, 'add_item.json')
-load_from_json_file('add_item.json')
