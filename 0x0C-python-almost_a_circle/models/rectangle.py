6c48b3ed2d0e016ae28a2da6a9b732f447faa924
chriswill88 <williechri79@gmail.com>
2019-10-20 04:54:35 -0400

    updated rectangle class: added area method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,10 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be >= 0")
             self.__y = y
+
+    def area(self):
+        return self.__width * self.__height
+
+    def display(self):
+        for i in range(self.__height):
+            print("#" * self.__width)
