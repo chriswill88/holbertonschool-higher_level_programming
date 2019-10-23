bd94ead5b331152cf03fd7b8ee2eba70fd0b378b
chriswill88 <williechri79@gmail.com>
2019-10-22 23:40:39 -0400

    task 16 complete

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,19 +23,31 @@ class Base:
         if list_dictionaries is None:
             return []
         return json.dumps(list_dictionaries)
+
     @classmethod
     def save_to_file(cls, list_objs):
         listy = []
+        new_list = []
+        newdict = {}
         extra = len("_Rectangle__")
         filename = cls.__name__ + ".json"
         for objects in list_objs:
             listy.append(objects.__dict__)
-        dicty = json.loads(json.dumps(listy))
+            dicty = json.loads(json.dumps(listy))
         for x in dicty:
+            # print("1 list", x)
             for i in x:
-		if len(i) > extra:
-			i = i[extra:]
-                print("dicty --", i, x[i])
-
+                if '_Rectangle__id' == i:
+                    continue
+                if len(i) > extra:
+                    name = i[extra:]
+                else:
+                    name = i
+                newdict[name] = x[i]
+                # print("dicty --", name, x[i])
+            new_list.append(dict(newdict))
+            
+        # print("the new dict -->", newdict)
+        # print(new_list)
         with open(filename, 'w') as f:
-            f.write(Base.to_json_string(listy))
+            f.write(Base.to_json_string(new_list))
