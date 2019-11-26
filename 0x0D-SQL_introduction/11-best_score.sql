5e9be1120f455f642e9623b2367e24065176d3be
chriswill88 <williechri79@gmail.com>
2019-11-25 23:49:49 -0500

    [ADD] task 11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- list all records, results both score and the name, ordered by score
-SELECT score, name FROM second_table ORDER BY SCORE DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY SCORE DESC;
