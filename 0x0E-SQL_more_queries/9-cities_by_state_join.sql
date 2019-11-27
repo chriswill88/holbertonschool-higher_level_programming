1d131171e1a6566dc8a145262c81bcd83a0cd3d2
chriswill88 <williechri79@gmail.com>
2019-11-27 03:11:17 -0500

    [Fix] task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- task 9 lists all ciries in the database
-SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa.cities JOIN hbtn_0d_usa.states ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ORDER BY cities.id ASC;
