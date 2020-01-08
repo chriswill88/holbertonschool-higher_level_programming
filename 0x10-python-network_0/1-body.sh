304bb828f0ea55d12786993aa64c654df7010342
chriswill88 <williechri79@gmail.com>
2020-01-07 20:50:32 -0500

    [ADD] task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this script only displays url if status code of 200 is returned
-if [ $(curl -X GET -I $1 -s | head -1 | cut -d " " -f 2) == 200 ]; then curl $1; else echo 0; fi
+if [ $(curl -X GET -I $1 -s | head -1 | cut -d " " -f 2) == 200 ]; then curl $1; else :; fi
