e8d22482efa41af58836f614e551b6b886b925e9
chriswill88 <williechri79@gmail.com>
2020-01-14 11:31:37 -0500

    [FIX] used .format

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
             for i in response:
                 print(i.decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print(e.code)
+        print("Error code: {}".format(e.code))
