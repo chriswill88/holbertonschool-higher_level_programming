7db8e67ae0806abbcce15ddc6734169cabcc14bc
chriswill88 <williechri79@gmail.com>
2020-01-14 00:32:27 -0500

    [ADD] task 9

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+"""Starwars search"""
+search = {'search': sys.argv[1]}
+
+r = requests.get('https://swapi.co/api/people', search)
+# print(r.json()['results'])
+stringies = r.json()['results']
+print("Number of results: {}".format(len(stringies)))
+for i in stringies:
+    print(i['name'])
+    # for x in i:
+    #     print(x['name'])
