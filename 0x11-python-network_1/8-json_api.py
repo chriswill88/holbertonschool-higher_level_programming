f0ef4b4be900b41a13654929f84f0845f5a0e111
chriswill88 <williechri79@gmail.com>
2020-01-14 15:10:42 -0500

    [FIX] task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,14 +2,14 @@
 import requests
 import sys
 
-"""The Search API"""
+"""The Searching the API"""
 if __name__ == "__main__":
-    try:
-        q = {'q': sys.argv[1]}
-    except Exception:
-        q = ""
+    q = ""
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
     r = requests.post(
-        'http://7737658283c4.19.hbtn-cod.io:5000/search_user', data=q)
+        'http://7737658283c4.19.hbtn-cod.io:5000/search_user', data={'q': q}
+        )
     try:
         print("[{}] {}".format(r.json()['id'], r.json()['name']))
     except Exception:
