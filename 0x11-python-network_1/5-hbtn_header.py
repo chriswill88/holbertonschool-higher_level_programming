d0f2a9b0e92cd85f768c3d1a86866624ededbb40
chriswill88 <williechri79@gmail.com>
2020-01-14 01:31:26 -0500

    [ADD]

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,5 +4,5 @@ import sys
 
 """Fetches a url and displays the variable X-Request"""
 if __name__ == "__main__":
-    r = requests.post(sys.argv[1])
+    r = requests.get(sys.argv[1])
     print("{}".format(r.headers['X-Request-Id']))
