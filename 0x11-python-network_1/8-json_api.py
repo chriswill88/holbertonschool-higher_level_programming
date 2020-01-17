46e337f6a583deee80661c968c1934b07c75a95c
chriswill88 <williechri79@gmail.com>
2020-01-17 10:39:58 -0500

    [FIX] url given

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,11 +7,12 @@ if __name__ == "__main__":
     q = ""
     if len(sys.argv) > 1:
         q = sys.argv[1]
+
     r = requests.post(
-        'http://7737658283c4.19.hbtn-cod.io:5000/search_user', data={'q': q}
+        'http://0.0.0.0:5000/search_user', data={'q': q}
         )
     try:
-        print("[{}] {}".format(r.json()['id'], r.json()['name']))
+        print("[{}] {}".format(r.json['id'], r.json['name']))
     except Exception:
         if r.json() == {}:
             print("No result")
