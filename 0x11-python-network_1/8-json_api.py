a28e3464210a66a26e41d5536e054eb8d68fb5fa
chriswill88 <williechri79@gmail.com>
2020-01-17 13:13:25 -0500

    [FIX] 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,17 +4,18 @@ import sys
 
 """The Searching the API"""
 if __name__ == "__main__":
-    q = ""
     if len(sys.argv) > 1:
         q = sys.argv[1]
+    else:
+        q = ""
 
     r = requests.post(
-        'http://0.0.0.0:5000/search_user', data={'q': q}
+        'http://473998d62070.19.hbtn-cod.io:5000/search_user', data={'q': q}
         )
     try:
         if r.json() == {}:
             print("No result")
         else:
-            print("[{}] {}".format(r.json['id'], r.json['name']))
+            print("[{}] {}".format(r.json().get('id'), r.json().get('name')))
     except Exception:
         print("Not a valid JSON")
