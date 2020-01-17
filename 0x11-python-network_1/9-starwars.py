9533791bcd0e95487df7ab0244825278137c3c16
chriswill88 <williechri79@gmail.com>
2020-01-17 10:15:42 -0500

    [FIX] task 9

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -5,15 +5,13 @@ import sys
 """Starwars search"""
 if __name__ == "__main__":
     search = {'search': sys.argv[1]}
-
     r = requests.get('https://swapi.co/api/people', search)
-    stringies = r.json()['results']
+    page = r.json()
+    stringies, count = page['results'], page['count']
     # print(type(r.json()))
     # for x, y in r.json().items():
     #     print("dict -> \n{}\n[{}]\n", x, y)
     # a bunch of info can be found in the json version of the request
-
-    count = r.json()['count']
     print("Number of results: {}".format(count))
-    for i in stringies:
-        print(i['name'])
+    for line in stringies:
+        print(line['name'])
