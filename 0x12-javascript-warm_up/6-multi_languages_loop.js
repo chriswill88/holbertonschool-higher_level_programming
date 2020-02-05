43a403167379e5dcb837b44ea6a16b0846e2e34a
chriswill88 <williechri79@gmail.com>
2020-02-04 21:27:58 -0500

    [ADD] task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+/* indentation is two spaces
+ * you can use in and of with a for loop
+ */
+
+for (const x of myVar) {
+  console.log(x);
+}
