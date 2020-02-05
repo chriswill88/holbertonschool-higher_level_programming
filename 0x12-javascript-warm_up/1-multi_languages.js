ec169322f484de257bd5a098d8bd2a618592b502
chriswill88 <williechri79@gmail.com>
2020-02-04 19:08:37 -0500

    [FIX] task 1: made it ss compliant

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+/* indentation is two spaces
+ * you can use in and of with a for loop
+ */
 
-for (x of myVar) {
-	console.log(x);
+for (const x of myVar) {
+  console.log(x);
 }
