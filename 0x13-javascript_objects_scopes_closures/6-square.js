5cfe82796991298ab17994101ae1faffa95a7af7
chriswill88 <williechri79@gmail.com>
2020-02-10 22:58:24 -0500

    [FIX] instatious function

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,16 +4,15 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    this.charPrint = function(c) {
+    this.charPrint = function (c) {
       if (!c) {
-        c = "X";
+        c = 'X';
       }
       for (let i = 0; i < size; i++) {
         console.log(c.repeat(size));
       }
-    }
+    };
   }
-
 }
 
 module.exports = Square;
