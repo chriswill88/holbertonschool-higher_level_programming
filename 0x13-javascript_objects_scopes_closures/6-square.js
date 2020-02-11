25da06a1c2a0396c79675cf27d2343fbc76762ef
chriswill88 <williechri79@gmail.com>
2020-02-10 22:55:19 -0500

    [FIX] instance method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,17 +4,16 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-  }
-
-  charPrint (c) {
-    if (!c) {
-      c = "X";
-    }
-
-    for (let i = 0; i < this.width; i++) {
-      console.log(c.repeat(this.width));
+    this.charPrint = function(c) {
+      if (!c) {
+        c = "X";
+      }
+      for (let i = 0; i < size; i++) {
+        console.log(c.repeat(size));
+      }
     }
   }
+
 }
 
 module.exports = Square;
