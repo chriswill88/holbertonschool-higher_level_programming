79e0ee9e2e4afd3c58167dbbb257962aee9cfa9d
chriswill88 <williechri79@gmail.com>
2020-02-11 21:29:42 -0500

    [ADD]

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+const arg = process.argv[2];
+request.get(arg, function (err, response, body) {
+  if (err) {
+    console.error('error:', err);
+  } else {
+    console.log('Code:', response && response.statusCode);
+  }
+});
